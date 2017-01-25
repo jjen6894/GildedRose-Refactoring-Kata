0cbb14b8596630adcf2de0c7b8cf6f9deb6f4b6f
Jack <jack_6894@hotmail.com>
2017-01-25 13:07:38 +0000

    refactored tests

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -2,28 +2,24 @@ require File.join(File.dirname(__FILE__), '../lib/gilded_rose')
 
 
 describe GildedRose do
-
+  before do
+    @items = [Item.new("foo", 0, 0), Item.new("foo", 1, 1), Item.new("foo", -1, 4) ]
+    GildedRose.new(@items).update_quality()
+  end
   describe "#update_quality" do
+
     it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "foo"
+      expect(@items[0].name).to eq "foo"
     end
 
     it "items at the end of the day sell_in loses a value of one" do
-      items = [Item.new("foo", 1, 1)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].sell_in).to eq (0)
+      expect(@items[1].sell_in).to eq (0)
     end
     it "items at the end of the day quality decreases by one" do
-      items = [Item.new("foo", 1, 1)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq (0)
+      expect(@items[1].quality).to eq (0)
     end
     it "item once passed sell by date, quality degrades twice as fast" do
-      items = [Item.new("foo", -1, 4)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].quality).to eq(2)
+      expect(@items[2].quality).to eq(2)
     end
   end
 
